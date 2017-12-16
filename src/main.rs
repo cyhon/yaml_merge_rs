46e2035797b6117c7ad99dbaccf5fef078011cbb
linhaitao <linhaitao@finogeeks.com>
2017-12-16 15:49:59 +0800

    file op

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,17 @@
 extern crate clap;
-extern crate yaml_rust;
 
 mod cli;
-
-use std::fs::File;
-use std::io::prelude::*;
-use std::collections::BTreeMap;
-use yaml_rust::{YamlLoader, YamlEmitter, ScanError};
+mod yaml;
 
 fn main() {
     let matches = cli::build_cli().get_matches();
 
     if let Some(inputs) = matches.values_of("input") {
-        let mut contents = String::new();
-
         for input in inputs {
-            File::open(input)
-                .and_then(|mut file| file.read_to_string(&mut contents))
-                .unwrap();
-
-            let yaml = YamlLoader::load_from_str(&contents).unwrap();
-
-            println!("input: {:?}", yaml)
+            let y = yaml::read_from_file(input);
+            if let Some(output) = matches.value_of("output") {
+                yaml::write_to_file(output, &y[0]);
+            }
         }
-    }
-
-    if let Some(output) = matches.value_of("output") {
-        println!("Value of output: {}", output);
     }
 }
---
+++ src/yaml.rs
@@ -0,0 +1,28 @@
+extern crate yaml_rust;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::vec::Vec;
+use self::yaml_rust::{YamlLoader, YamlEmitter, Yaml};
+
+pub fn read_from_file(path: &str) -> Vec<Yaml> {
+    let mut contents = String::new();
+
+    File::open(path)
+        .and_then(|mut file| file.read_to_string(&mut contents))
+        .unwrap();
+
+    return YamlLoader::load_from_str(&contents).unwrap();
+}
+
+pub fn write_to_file(path: &str, data: &Yaml) {
+    let mut out_str = String::new();
+
+    {
+        let mut emitter = YamlEmitter::new(&mut out_str);
+        emitter.dump(data).unwrap();
+    }
+    File::create(path)
+        .and_then(|mut file| file.write_all(out_str.as_bytes()))
+        .unwrap();
+}
